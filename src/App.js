98a7c78268f31aab6389be00d3eed69124b42d6c
Vincent Kjellman <vincent.kjellman@changetheagency.se>
2022-01-11 22:20:45 +0100

    Temprarily remove particles

--- src/App.js
+++ src/App.js
@@ -13,78 +13,78 @@ const app = new Clarifai.App({
   apiKey: '72876add4fc5469998d8b1e86d2124a6'
 });
 
-// const particleOptions = {
-//   fpsLimit: 60,
-//   interactivity: {
-//     events: {
-//       onClick: {
-//         enable: true,
-//         mode: "repulse",
-//       },
-//       onHover: {
-//         enable: true,
-//         mode: "grab",
-//       },
-//       resize: true,
-//     },
-//     modes: {
-//       bubble: {
-//         distance: 400,
-//         duration: 2,
-//         opacity: 0.8,
-//         size: 40,
-//       },
-//       push: {
-//         quantity: 4,
-//       },
-//       repulse: {
-//         distance: 200,
-//         duration: 0.4,
-//       },
-//     },
-//   },
-//   particles: {
-//     color: {
-//       value: "#ffffff",
-//     },
-//     links: {
-//       color: "#ffffff",
-//       distance: 150,
-//       enable: true,
-//       opacity: 0.5,
-//       width: 1,
-//     },
-//     collisions: {
-//       enable: true,
-//     },
-//     move: {
-//       direction: "none",
-//       enable: true,
-//       outMode: "bounce",
-//       random: false,
-//       speed: 2,
-//       straight: false,
-//     },
-//     number: {
-//       density: {
-//         enable: true,
-//         area: 800,
-//       },
-//       value: 80,
-//     },
-//     opacity: {
-//       value: 0.5,
-//     },
-//     shape: {
-//       type: "circle",
-//     },
-//     size: {
-//       random: true,
-//       value: 2,
-//     },
-//   },
-//   detectRetina: true,
-// }
+const particleOptions = {
+  fpsLimit: 60,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "repulse",
+      },
+      onHover: {
+        enable: true,
+        mode: "grab",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 2,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 2,
+    },
+  },
+  detectRetina: true,
+}
  // console.log(response)
         // console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
 class App extends Component {
@@ -144,85 +144,14 @@ class App extends Component {
         onButtonSubmit={this.onButtonSubmit} 
       />
       <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
-
+{/* 
       <Particles 
         className="particles"
-        // id="tsparticles"
+        id="tsparticles"
         // init={particlesInit}
         // loaded={particlesLoaded}
-        options={
-          {
-          fpsLimit: 60,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "repulse",
-              },
-              onHover: {
-                enable: true,
-                mode: "grab",
-              },
-              resize: true,
-            },
-            modes: {
-              bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
-              },
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outMode: "bounce",
-              random: false,
-              speed: 2,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              random: true,
-              value: 2,
-            },
-          },
-          detectRetina: true,}}
-      />
+        options={particleOptions}
+      /> */}
     </div>
     )
   };
